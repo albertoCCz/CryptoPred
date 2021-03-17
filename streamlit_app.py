08d2a00190b215229bef5dea4aa80726bbfcf265
Akoss 95 <albertocalzada95@gmail.com>
2021-03-17 20:49:26 +0100

    Update app: now shows plot of selected data with all indicators

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from utils.utils import plot_data
 
 import pandas as pd
 import numpy as np
@@ -24,12 +25,12 @@ st.write(f'The data we are working with is {PAIRS[0]},\
 
 # Select a period of time
 st.sidebar.write('Select data range')
-init_time = st.sidebar.date_input(label='Initial datetime:',
+init_time = st.sidebar.date_input(label='Initial date:',
 	value=datetime.datetime(2020, 10, 29, 14, 30, 00),
 	min_value=pd.to_datetime(df.index.values[0]),
 	max_value=pd.to_datetime(df.index.values[-2]))
-final_time = st.sidebar.date_input(label='Last datetime:',
-	value=datetime.datetime(2020, 10, 29, 14, 30, 00),
+final_time = st.sidebar.date_input(label='Last date:',
+	value=datetime.datetime(2020, 10, 30, 14, 30, 00),
 	min_value=pd.to_datetime(df.index.values[1]),
 	max_value=pd.to_datetime(df.index.values[-1]))
 
@@ -50,6 +51,18 @@ df_day = select_data(init_time, final_time)
 # Show selected data
 show_selected_data = st.sidebar.checkbox(label='Show selected data', value=False)
 if show_selected_data:
-	st.write(df_day)
+	st.write(df_day.loc[:, :'volume'])
 else:
 	st.write('To start, select the data range in the left sidebar.')
+
+
+# PLOT
+# ============================================
+plot_bb_bands = True
+
+# Show plot
+plot_selected_data = st.sidebar.checkbox(label='Plot data', value=False)
+if plot_selected_data:
+	st.write('Plot of the data in the selected period:')
+	fig = plot_data(df_day, plot_bb_bands)
+	st.pyplot(fig=fig)
